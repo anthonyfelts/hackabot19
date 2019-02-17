65619658dcd1de564eeb53eed6461f0abfc699e4
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-02-17 14:16:50 -0800

    client added

---
+++ sketch_feb17e.ino
@@ -0,0 +1,22 @@
+#include <DistanceDetect.h>
+
+const int ledPin = 13;
+dist DistanceDetect(8,7);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i = dist.checkDist();
+  if (i > 5) {
+      digitalWrite(ledPin, HIGH);
+  }
+  else {
+      digitalWrite(ledPin, LOW);
+  }
+  delay(250);
+}
