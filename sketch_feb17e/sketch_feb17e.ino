e2c9bc09539a64fadfea5779e5edf71028b927af
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-02-17 17:35:03 -0800

    fix client

--- sketch_feb17e/sketch_feb17e.ino
+++ sketch_feb17e/sketch_feb17e.ino
@@ -1,7 +1,6 @@
 #include "DistanceDetect.h"
 #include "Motor.h"
 
-const int ledPin = 13;
 DistanceDetect dist(7,8);
 Motor motor_1(5,6);
 Motor motor_2(3,11);
@@ -13,14 +12,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (dist.checkDist() < 5) {
-    delay(15);
-    motor_1.move(0);
-    motor_2.move(0);
-    return;
-  }
-  else {
-    motor_1.move(-100);
-    motor_2.move(-100);
-  }
+  motor_1.move(150);
+  motor_2.move(150);
+  delay(1000);
+  motor_1.move(-150);
+  motor_2.move(-150);
+  delay(1000);
 }
